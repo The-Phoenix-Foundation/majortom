885a228e2b322298454c19d93c6eabac2d9e8976
ap-- <andreas@poehlmann.io>
2019-10-19 17:05:48 +0200

    Revert "fix local dev"
    This reverts commit 8dfc729b07d2b06a9c450af0a1e91be869ac9dee.

--- phoenix/app.py
+++ phoenix/app.py
@@ -5,7 +5,7 @@ from flask import Flask, g, render_template, url_for, request, make_response
 def create_app():
     app = Flask(__name__)
 
-    from .main.views import blueprint as main_blueprint
+    from main.views import blueprint as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
