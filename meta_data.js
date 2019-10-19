66bf964c416e39ef17a957820109c9ba90f35c0f
Falko Hofmann <falkohofmann@gmail.com>
2019-10-19 22:35:21 +0200

    fixed link suffix

--- meta_data.js
+++ meta_data.js
@@ -3,5 +3,5 @@ function get_celestrak_url(internal_designator) {
     //internal designator id looks like 1964-063B
     //data example https://celestrak.com/satcat/1964/1964-063.php#C
     [year, sub_id] = internal_designator.split("-");
-    return "https://celestrak.com/satcat/" + year + "/" + internal_designator.slice(0, -1) + ".php#" + internal_designator.slice(0, -1);
+    return "https://celestrak.com/satcat/" + year + "/" + internal_designator.slice(0, -1) + ".php#" + internal_designator.slice(-1);
 }
