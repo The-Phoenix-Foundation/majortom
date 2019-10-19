f6d4f9e93e5373751a8a13d078451dbd080fe0ab
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2019-10-19 22:10:13 +0200

    add shiny deployment

---
+++ shiny/dependencies.R
@@ -0,0 +1,4 @@
+# install dependencies for shiny
+install.packages(c('shiny', 'tidyverse', 'glue', 'devtools', 'lubridate'), ask=FALSE)
+devtools::install_github('ricardo-bion/ggradar', dependencies = TRUE)
+
