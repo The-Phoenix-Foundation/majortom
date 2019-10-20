f870e4f86528eff53dadfb7aa3ee1caed3a3ecf9
Uemit Seren <Uemit Seren uemit.seren@gmail.com>
2019-10-20 10:08:42 +0200

    add model

---
+++ popup.js
@@ -0,0 +1,32 @@
+var modelwwd;
+var modelLayer = new WorldWind.RenderableLayer("satellites");
+//modelwwd.goTo(userLocation);
+var selectedSatellite;
+
+function showPopup(userObject) {
+    $('#satelliteName').text(userObject.displayName);
+    $('#satelliteModal').modal();
+    console.log(userObject);
+    selectedSatellite = satelliteObjects[userObject.displayName]['satellite']
+    renderSatelliteModel()
+
+}
+
+function renderSatelliteModel() {
+    if (!modelwwd) 
+       return;
+    var satellite = selectedSatellite;
+    modelLayer.removeAllRenderables();
+    var currentSatelitePosition = currentSatellitePos[satellite.id];
+    var position = new WorldWind.Position(currentSatelitePosition.latitude, currentSatelitePosition.longitude, currentSatelitePosition.altitude);
+    modelwwd.goto(position)
+    var colladaLoader = new WorldWind.ColladaLoader(position);
+    var category = satellite.category;
+    colladaLoader.init({dirPath: './models/'+category+'/'});
+    var scene = colladaLoader.parse(satelliteModelMap[category]);
+    scene.displayName = satellite.id;
+    scene.scale = 10000;
+    scene.position = position;
+    scene.altitudeMode = WorldWind.ABSOLUTE;
+    modelLayer.addRenderable(scene);
+}
